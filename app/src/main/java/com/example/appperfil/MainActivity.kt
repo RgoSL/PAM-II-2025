810a12e5fb70c29a27401fd4e88a8b8044790240
Diggo 🤙🗿🤙 <125403873+RgoSL@users.noreply.github.com>
2025-03-10 11:38:56 -0300

    Segundo Projeto Pronto

--- app/src/main/java/com/example/appperfil/MainActivity.kt
+++ app/src/main/java/com/example/appperfil/MainActivity.kt
@@ -3,72 +3,131 @@ package com.example.appperfil
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.appperfil.ui.theme.AppPerfilTheme
 import com.example.appperfil.ui.theme.RoxoClaro
-import com.example.appperfil.ui.theme.RoxoEscuro
-
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AppPerfilTheme {
-                }
+                Forms()
             }
         }
     }
+}
 
 @Composable
-fun perfil(){
-    Column (
+fun Forms() {
+    val gradientBrush = Brush.verticalGradient(
+        colors = listOf(RoxoClaro, Color.Cyan)
+    )
 
-    ) {
-      Box(
-        modifier = Modifier
-            .background(
-                brush = Brush.horizontalGradient(
-                    colors = listOf(
-                        RoxoClaro, RoxoEscuro
-                    )
-                )
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(gradientBrush),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Imagem Circular",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
             )
-            .size(100.dp),
-          contentAlignment = Alignment.Center
-      ) {
-          Image(
-              painter = painterResource(R.drawable.medo),
-              contentDescription = "Imagem do Perfil",
-              modifier = Modifier
-                  .size(90.dp)
-                  .clip(CircleShape)
+        }
 
-          )
+        Spacer(modifier = Modifier.height(16.dp))
 
-      }
+        InputField(label = "Nome")
+        Spacer(modifier = Modifier.height(8.dp))
+        InputField(label = "Email")
+        Spacer(modifier = Modifier.height(8.dp))
+        InputField(label = "Telefone")
+        Spacer(modifier = Modifier.height(8.dp))
+        InputField(label = "Endereço")
 
+        Spacer(modifier = Modifier.height(36.dp))
+
+        CustomButton(label = "Salvar")
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
+fun InputField(label: String) {
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
 
-fun PerfilPreview(){
-    perfil()
+    OutlinedTextField(
+        value = textState.value,
+        onValueChange = { textState.value = it },
+        label = { Text(text = label) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = 16.dp),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Blue,
+            unfocusedBorderColor = Color.Gray,
+            focusedLabelColor = Color.Blue,
+            unfocusedLabelColor = Color.Gray,
+
+            cursorColor = Color.Blue,
+
+        )
+    )
 }
 
+@Composable
+fun CustomButton(label: String) {
+    Button(
+        onClick = { },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .height(50.dp),
+        content = {
+            Text(
+                text = label,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.CenterVertically),
+                style = androidx.compose.ui.text.TextStyle(fontSize = 16.sp)
+            )
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppPerfilTheme {
+        Forms()
+    }
+}
