b8269fc805543bbb8427eb5884cf93dd71d8bd63
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-04-07 00:57:39 -0300

    Add o App em Andamento
    Ainda há uma classe faltando

---
+++ app/src/main/java/com/navigation/SetupNavGraph.kt
@@ -0,0 +1,27 @@
+package com.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.example.appnavigate.ui.screen.LoginScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screens.LoginScreen.route) {
+        composable(route = Screens.LoginScreen.route) {}
+        LoginScreen(navController = navController)
+    }
+
+    composable(route = Screens.HomeScreen.route, arguments = listOf(
+        navArgument(USER) {
+            type = NavType.StringType
+            defaultValue = ""
+            nullable = true
+        }
+    )) {
+        HomeRoute(userName = it.arguments?.getString(USER) ?: "")
+    }
+}
+
